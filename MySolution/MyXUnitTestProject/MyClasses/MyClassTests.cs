b6677016ad4980f6aaf778386e57ee85181786e7
SSeelos <sebastian.seelos@gmx.de>
2023-04-13 22:07:43 +0200

    xunit test proj

---
+++ MySolution/MyXUnitTestProject/MyClasses/MyClassTests.cs
@@ -0,0 +1,13 @@
+﻿namespace MyClassLibraryNET.Tests
+{
+    public class MyClassTests
+    {
+        [Fact()]
+        public void MyClassTest()
+        {
+            var myClass = new MyClass();
+
+            myClass.Should().NotBeNull();
+        }
+    }
+}
