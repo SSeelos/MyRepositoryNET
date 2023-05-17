5902b333f0e7c32a85fd0780d06d80c75d3a05cd
SSeelos <sebastian.seelos@gmx.de>
2023-05-17 09:14:42 +0200

    generic class

---
+++ MySolution/MyClassLibraryNET/MyClasses/MyGenericClass.cs
@@ -0,0 +1,11 @@
+﻿namespace MyRootNamespace.MyClassLibraryNET
+{
+    public class MyGenericClass<T>
+    {
+        public T MyValue { get; set; }
+        public MyGenericClass(T value)
+        {
+            MyValue = value;
+        }
+    }
+}
---
+++ MySolution/MyXUnitTestProject/MyClasses/MyGenericClassTests.cs
@@ -0,0 +1,12 @@
+﻿namespace MyRootNamespace.MyClassLibraryNET.Tests
+{
+    public class MyGenericClassTests
+    {
+        [Fact()]
+        public void MyGenericClassTest()
+        {
+            var instance = new MyGenericClass<string>("myValue");
+            instance.MyValue.Should().Be("myValue");
+        }
+    }
+}
