2a0c0cfe9a021892094302b4fc36379d5e409e7f
SSeelos <sebastian.seelos@gmx.de>
2023-06-15 16:28:41 +0200

    multiple targetframeworks

---
+++ MySolution/MyLibraryTargetframeworks/FrameworkTest.cs
@@ -0,0 +1,16 @@
+﻿namespace MyLibraryTargetframeworks
+{
+    public static class FrameworkTest
+    {
+        public static string GetTargetFramework()
+        {
+#if NETSTANDARD2_1
+            return "NETSTANDARD2_1";
+#endif
+
+#if NET48_OR_GREATER
+            return "NET48_OR_GREATER";
+#endif
+        }
+    }
+}
---
+++ MySolution/MyLibraryTargetframeworksTests/Tests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace MyLibraryTargetframeworks.Tests
+{
+    public class Tests
+    {
+        [Fact()]
+        public void GetTargetFrameworkTest()
+        {
+            var res = FrameworkTest.GetTargetFramework();
+#if NETSTANDARD2_1
+            Assert.Equal("NETSTANDARD2_1", res);
+#endif
+#if NET48_OR_GREATER
+            Assert.Equal("NET48_OR_GREATER", res);
+#endif
+        }
+    }
+}
