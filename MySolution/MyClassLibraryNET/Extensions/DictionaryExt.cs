e49bc569f4abb8aa38a459e56477620c0592aeac
SSeelos <sebastian.seelos@gmx.de>
2023-06-15 16:29:32 +0200

    dict extenion allow space and ignore case

---
+++ MySolution/MyClassLibraryNET/Extensions/DictionaryExt.cs
@@ -0,0 +1,16 @@
+﻿namespace MyRootNamespace.MyClassLibraryNET
+{
+    public static class DictionaryExt
+    {
+        /// <summary>
+        /// ignore white space and case
+        /// </summary>
+        public static TVal GetGenerous<TVal>(this Dictionary<string, TVal> dictionary, string key)
+        {
+            return dictionary.Where(x
+                => x.Key.Replace(" ", "").ToLower()
+                == key.Replace(" ", "").ToLower())
+                .FirstOrDefault().Value;
+        }
+    }
+}
---
+++ MySolution/MyXUnitTestProject/Extensions/DictionaryExtTests.cs
@@ -0,0 +1,23 @@
+﻿namespace MyRootNamespace.MyClassLibraryNET.Tests
+{
+    public class DictionaryExtTests
+    {
+        [Fact()]
+        public void GetGenerousTest()
+        {
+            var dict = new Dictionary<string, string>
+            {
+                { "key1", "value1" },
+                { "key 2", "value2" }
+            };
+
+            dict.GetGenerous("key1").Should().Be("value1");
+            dict.GetGenerous("key 1").Should().Be("value1");
+            dict.GetGenerous("Key1").Should().Be("value1");
+            dict.GetGenerous("key2").Should().Be("value2");
+            dict.GetGenerous("key 2").Should().Be("value2");
+            dict.GetGenerous("Key 2").Should().Be("value2");
+            dict.GetGenerous("nope").Should().Be(null);
+        }
+    }
+}
