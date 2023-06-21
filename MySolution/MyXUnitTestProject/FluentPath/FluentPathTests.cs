11b3e41c16a3b5acdc80b1ad20e2cbdf14e27871
SSeelos <sebastian.seelos@gmx.de>
2023-06-21 17:06:37 +0200

    todo fluent path tests

---
+++ MySolution/MyXUnitTestProject/FluentPath/FluentPathTests.cs
@@ -0,0 +1,42 @@
+﻿//todo
+
+//using FluentAssertions.Execution;
+//using Path = Fluent.IO.Path;
+
+//namespace MyXUnitTestProject
+//{
+//    public class FluentPathTests
+//    {
+
+//        [Fact]
+//        public void Function()
+//        {
+//            string path = @"C:\Users\sse\source\repos\MyWixInstallerProject\TestProject\FluentPath\FluentPathTests.cs";
+//            var p = Path.Get(path);
+
+//            using (new AssertionScope())
+//            {
+//                p.Exists.Should().BeTrue();
+//                p.GetDirectoryInfo().Exists
+//                    .Should().BeTrue();
+//                p.GetFileInfo().Exists
+//                    .Should().BeTrue();
+
+//                p.IsRooted.Should().BeTrue();
+//                p.IsDirectory.Should().Be(false);
+//                p.DirectoryName
+//                    .Should().Be(@"C:\Users\sse\source\repos\MyWixInstallerProject\TestProject\FluentPath");
+//                p.FileName
+//                    .Should().Be("FluentPathTests.cs");
+//                p.FileNameWithoutExtension
+//                    .Should().Be("FluentPathTests");
+//                p.FullPath
+//                    .Should().Be(path);
+//                p.Extension
+//                    .Should().Be(".cs");
+
+//            }
+//        }
+
+//    }
+//}
