7ff386ddc1b1f34e3fd51a950ae80da11b5f1c2a
SSeelos <sebastian.seelos@gmx.de>
2023-06-28 15:52:00 +0200

    incremental generator (todo)

---
+++ MySolution/MyClassLibraryNetStandard2_0/SourceGenerators/MyIncrementalGenerator.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace MyClassLibraryNetStandard2_0
+{
+    [Generator]
+    public class MyIncrementalGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            //execution via a series of transformations
+
+        }
+    }
+}
