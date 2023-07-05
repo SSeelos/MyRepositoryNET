6e48610cffdcaef8547007cdcdaf549582883e76
SSeelos <sebastian.seelos@gmx.de>
2023-07-05 16:51:19 +0200

    classfixture collection

---
+++ MySolution/MyXUnitTestProject/XUnit/SharedContext/ClassFixtureCollection/MyClassCollectionTestsA.cs
@@ -0,0 +1,31 @@
+﻿namespace MyXUnitTestProject
+{
+    [Collection(nameof(MyClassFixtureCollection))]
+    public class MyClassCollectionTestsA
+    {
+        MyFixtureA myFixtureA;
+        MyFixtureB myFixtureB;
+        public MyClassCollectionTestsA(MyFixtureA myFixtureA, MyFixtureB myFixtureB)
+        {
+            this.myFixtureA = myFixtureA;
+            this.myFixtureB = myFixtureB;
+        }
+
+        [Fact]
+        public void MyFactA()
+        {
+            var resultA = myFixtureA.Data + nameof(MyFactA);
+            var resultB = myFixtureB.Data + nameof(MyFactA);
+            Assert.Equal(nameof(MyFixtureA) + "DataMyFactA", resultA);
+            Assert.Equal(nameof(MyFixtureB) + "DataMyFactA", resultB);
+        }
+        [Fact]
+        public void MyFactB()
+        {
+            var resultA = myFixtureA.Data + nameof(MyFactB);
+            var resultB = myFixtureB.Data + nameof(MyFactB);
+            Assert.Equal(nameof(MyFixtureA) + "DataMyFactB", resultA);
+            Assert.Equal(nameof(MyFixtureB) + "DataMyFactB", resultB);
+        }
+    }
+}
---
+++ MySolution/MyXUnitTestProject/XUnit/SharedContext/ClassFixtureCollection/MyClassFixtureCollection.cs
@@ -0,0 +1,13 @@
+﻿namespace MyXUnitTestProject
+{
+    /// <summary>
+    /// will never be created,
+    /// treats each test class of this collection as if it implements <see cref="IClassFixture{TFixture}"/>,
+    /// </summary>
+    [CollectionDefinition(nameof(MyClassFixtureCollection))]
+    public class MyClassFixtureCollection :
+        IClassFixture<MyFixtureA>,
+        IClassFixture<MyFixtureB>
+    {
+    }
+}
