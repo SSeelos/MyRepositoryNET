1bcc162c7ec28899f6847ded13a81b067c82e5d0
SSeelos <sebastian.seelos@gmx.de>
2023-07-05 16:23:59 +0200

    collectiondef tests

---
+++ MySolution/MyXUnitTestProject/XUnit/SharedContext/CollectionFixtures/MyCollectionFixtures.cs
@@ -0,0 +1,14 @@
+﻿namespace MyXUnitTestProject
+{
+    /// <summary>
+    /// will never be created,
+    /// but will be used to pass shared instances of <see cref="MyFixtureA"/> and <see cref="MyFixtureB"/>
+    /// to the constructors of classes that use this collection fixture.
+    /// </summary>
+    [CollectionDefinition(nameof(MyCollectionFixtures))]
+    public class MyCollectionFixtures :
+        ICollectionFixture<MyFixtureA>,
+        ICollectionFixture<MyFixtureB>
+    {
+    }
+}
---
+++ MySolution/MyXUnitTestProject/XUnit/SharedContext/CollectionFixtures/MyCollectionTests.cs
@@ -0,0 +1,33 @@
+﻿namespace MyXUnitTestProject
+{
+    [Collection(nameof(MyCollectionFixtures))]
+    public class MyCollectionTests
+    {
+        MyFixtureA myFixtureA;
+        MyFixtureB myFixtureB;
+        public MyCollectionTests(MyFixtureA myFixtureA, MyFixtureB myFixtureB)
+        {
+            this.myFixtureA = myFixtureA;
+            this.myFixtureB = myFixtureB;
+        }
+
+
+        [Fact]
+        public void MyFactA()
+        {
+            var resultA = myFixtureA.Data + nameof(MyFactA);
+            var resultB = myFixtureB.Data + nameof(MyFactA);
+            Assert.Equal(nameof(MyFixtureA) + "DataMyFactA", resultA);
+            Assert.Equal(nameof(MyFixtureB) + "DataMyFactA", resultB);
+        }
+        [Fact]
+        public void MyFactB()
+        {
+            var resultA = myFixtureA.Data + nameof(MyFactB);
+            var resultB = myFixtureB.Data + nameof(MyFactB);
+            Assert.Equal(nameof(MyFixtureA) + "DataMyFactB", resultA);
+            Assert.Equal(nameof(MyFixtureB) + "DataMyFactB", resultB);
+        }
+
+    }
+}
