5750a1ef99bf40e38852a765ca00c9689dc73d08
SSeelos <sebastian.seelos@gmx.de>
2023-07-05 11:01:26 +0200

    assertion scope ext

---
+++ MySolution/MyXUnitTestProject/FluentAssertions/AssertionScopeExt.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions.Execution;
+namespace MyXUnitTestProject
+{
+    public static class AssertionScopeExt
+    {
+        public static void AssertionScope<T>(this T subject, Action<T> action)
+        {
+            using var scope = new AssertionScope();
+            action.Invoke(subject);
+        }
+        /// <summary>
+        /// reports failures BUT does not cause test to fail!
+        /// </summary>
+        public static string[] AssertionScopeFailures<T>(this T subject, Action<T> action)
+        {
+            using var scope = new AssertionScope();
+            action.Invoke(subject);
+
+            return scope.Discard();
+        }
+    }
+}
---
+++ MySolution/MyXUnitTestProject/FluentAssertions/AssertionScopeTest.cs
@@ -0,0 +1,32 @@
+﻿namespace MyXUnitTestProject
+{
+    public class AssertionScopeTest
+    {
+
+        [Fact]
+        public void Scope()
+        {
+            var t = "t";
+            t.AssertionScope(t =>
+            {
+                t.Should().Be("t");
+                t.Should().NotBe("fail");
+            });
+        }
+
+        [Fact]
+        public void ScopeFailures()
+        {
+            var test = "test";
+
+            var failures = test.AssertionScopeFailures(t =>
+            {
+                t.Should().Be("test");
+                t.Should().Be("fail");
+                t.Should().Be("fail2");
+            });
+
+            failures.Should().HaveCount(2);
+        }
+    }
+}
