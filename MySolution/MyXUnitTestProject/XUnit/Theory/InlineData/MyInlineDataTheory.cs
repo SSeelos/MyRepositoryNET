f6865f26ec99028fed226572b92d40508a940645
SSeelos <sebastian.seelos@gmx.de>
2023-07-06 10:42:43 +0200

    inlinedata

---
+++ MySolution/MyXUnitTestProject/XUnit/Theory/InlineData/MyInlineDataTheory.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions.Execution;
+namespace MyXUnitTestProject
+{
+    public class MyInlineDataTheory
+    {
+        [Theory]
+        [InlineData(1, 2)]
+        public void MyTheory(double paramA, double paramB)
+        {
+            using (new AssertionScope())
+            {
+                paramA.Should().Be(1);
+                paramB.Should().Be(2);
+            }
+        }
+
+    }
+}
