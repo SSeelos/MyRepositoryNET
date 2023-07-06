4b18d8b5503b2e1298b299d9f22d60e628d6e137
SSeelos <sebastian.seelos@gmx.de>
2023-07-06 10:05:10 +0200

    theory classdata

---
+++ MySolution/MyXUnitTestProject/XUnit/Theory/ClassData/MyClassDataA.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace MyXUnitTestProject
+{
+    public class MyClassDataA : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+            => new List<object[]>()
+            {
+                new object[] { 1,"1A"},
+                new object[] { 2,"2A"},
+            }
+            .GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+    }
+}
---
+++ MySolution/MyXUnitTestProject/XUnit/Theory/ClassData/MyClassDataTheory.cs
@@ -0,0 +1,13 @@
+﻿namespace MyXUnitTestProject
+{
+    public class MyClassDataTheory
+    {
+        [Theory]
+        [ClassData(typeof(MyClassDataA))]
+        public void MyTheory(int id, string data)
+        {
+            id.Should().BeOneOf(1, 2);
+            data.Should().BeOneOf("1A", "2A");
+        }
+    }
+}
