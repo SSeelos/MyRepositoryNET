39447c2c10ff3e8ddc8fde643a1a34f394eb38f8
SSeelos <sebastian.seelos@gmx.de>
2023-07-06 14:51:55 +0200

    mock returns test

---
+++ MySolution/MyXUnitTestProject/Moq/MockTests.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using MyRootNamespace.MyClassLibraryNET;
+namespace MyXUnitTestProject;
+
+public class MockTests
+{
+    [Fact]
+    public void MockReturn()
+    {
+        var mock = new Mock<IMyInterfaceOut>();
+        mock.Setup(_ => _.MyInterfaceMethod())
+            .Returns("mockData");
+
+        mock.Object.MyInterfaceMethod()
+            .Should().Be("mockData");
+    }
+
+}
