120c9639ec009ffe327fce3fd96750f6badb03c6
SSeelos <sebastian.seelos@gmx.de>
2023-07-06 10:43:32 +0200

    added autoFixture

---
+++ MySolution/MyXUnitTestProject/XUnit/AutoFixture/MyAutoData.cs
@@ -0,0 +1,16 @@
+﻿using AutoFixture;
+
+namespace MyXUnitTestProject
+{
+    public class MyAutoData
+    {
+        [Theory, AutoData]
+        public void MyAutoDataTheory(MyClassDataA myclass)
+        {
+            myclass.Should().NotBeNull();
+            myclass.AsEnumerable().Should()
+                .ContainEquivalentOf(new object[] { 1, "1A" }).And
+                .ContainEquivalentOf(new object[] { 2, "2A" });
+        }
+    }
+}
