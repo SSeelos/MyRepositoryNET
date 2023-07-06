08cda175e40727448d6b8db40d584a32713c3ff8
SSeelos <sebastian.seelos@gmx.de>
2023-07-06 11:08:45 +0200

    inlineautodata

--- MySolution/MyXUnitTestProject/XUnit/AutoFixture/MyAutoData.cs
+++ MySolution/MyXUnitTestProject/XUnit/AutoFixture/MyAutoData.cs
@@ -12,5 +12,18 @@ namespace MyXUnitTestProject
                 .ContainEquivalentOf(new object[] { 1, "1A" }).And
                 .ContainEquivalentOf(new object[] { 2, "2A" });
         }
+
+        [Theory, InlineAutoData(1, "2", 3.0)]
+        public void MyInlineAutoDataTheory(int id, string data, double value, MyClassDataA myclass)
+        {
+            id.Should().Be(1);
+            data.Should().Be("2");
+            value.Should().Be(3.0);
+
+            myclass.Should().NotBeNull();
+            myclass.AsEnumerable().Should()
+                .ContainEquivalentOf(new object[] { 1, "1A" }).And
+                .ContainEquivalentOf(new object[] { 2, "2A" });
+        }
     }
 }
