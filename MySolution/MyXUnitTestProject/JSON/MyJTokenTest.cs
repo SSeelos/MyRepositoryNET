62994d230dbfc862bb581fc29e5de35ecdb4c7e8
SSeelos <sebastian.seelos@gmx.de>
2023-07-17 16:49:48 +0200

    jToken tests

---
+++ MySolution/MyXUnitTestProject/JSON/MyJTokenTest.cs
@@ -0,0 +1,25 @@
+﻿using MyRootNamespace.MyClassLibraryNET;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MyXUnitTestProject
+{
+    public class MyJTokenTest
+    {
+        [Fact]
+        public void FromObjectTest()
+        {
+            JToken jToken = JToken.FromObject(new { MyPropertyA = "myValueA" });
+            jToken.Should().NotBeNull();
+            jToken.Type.Should().Be(JTokenType.Object);
+        }
+        [Fact]
+        public void FromObjectSerializerTest()
+        {
+            var serializer = new JsonSerializer();
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+            JToken jToken = JToken.FromObject(new MyClass { MyAutoProperty = null, MyFullProperty = "myValue" }, serializer);
+            jToken.Should().NotBeNull();
+        }
+    }
+}
