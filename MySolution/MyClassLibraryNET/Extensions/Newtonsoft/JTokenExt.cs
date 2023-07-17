7f23b5b68c29e93be56f7f1fd3ef11782e3db900
SSeelos <sebastian.seelos@gmx.de>
2023-07-17 16:43:44 +0200

    jtoken ext GetValue

---
+++ MySolution/MyClassLibraryNET/Extensions/Newtonsoft/JTokenExt.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace MyRootNamespace.MyClassLibraryNET
+{
+    public static class JTokenExt
+    {
+        public static T? GetValue<T>(this JToken token, string tokenName)
+        {
+            var value = token.SelectToken(tokenName);
+            return value == null ? default : value.Value<T>();
+        }
+    }
+}
---
+++ MySolution/MyXUnitTestProject/Extensions/Newtonsoft/JTokenExt.cs
@@ -0,0 +1,23 @@
+﻿using MyRootNamespace.MyClassLibraryNET;
+using Newtonsoft.Json.Linq;
+
+namespace MyXUnitTestProject
+{
+    public class JTokenExt
+    {
+        [Fact]
+        public void GetValue_StringTest()
+        {
+            var jObj = JObject.Parse("{'myString':'myValue'}");
+            var value = jObj.GetValue<string>("myString");
+            value.Should().Be("myValue");
+        }
+        [Fact]
+        public void GetValue_ArrayTest()
+        {
+            var jObj = JObject.Parse("{myArray: ['A','B']}");
+            var value = jObj.GetValue<JArray>("myArray");
+        }
+
+    }
+}
