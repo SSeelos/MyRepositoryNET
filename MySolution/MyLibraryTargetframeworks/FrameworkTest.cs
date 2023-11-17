29f392f9d0b573af54f8f8f8c2ada4bf38b76d58
SSeelos <sebastian.seelos@gmx.de>
2023-11-17 13:25:59 +0100

    target frameworks

--- MySolution/MyLibraryTargetframeworks/FrameworkTest.cs
+++ MySolution/MyLibraryTargetframeworks/FrameworkTest.cs
@@ -4,12 +4,18 @@
     {
         public static string GetTargetFramework()
         {
+#if NETSTANDARD2_0
+            return "NETSTANDARD2_0";
+#endif
 #if NETSTANDARD2_1
             return "NETSTANDARD2_1";
 #endif
 
 #if NET48_OR_GREATER
             return "NET48_OR_GREATER";
+#endif
+#if NET5_0_OR_GREATER
+            return "NET5_0_OR_GREATER";
 #endif
         }
     }
